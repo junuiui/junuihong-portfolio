151e853a413fb6cd5398d6037db3b38eccc66449
Junui Hong <jha357@sfu.ca>
2025-12-05 10:50:30 -0800

    google analytics added

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (window.gtag) {
+            window.gtag("config", "G-XXXX", {
+                page_path: location.pathname + location.search,
+            });
+        }
+    }, [location]);
     return (
         <>
             <MainNavigation />
